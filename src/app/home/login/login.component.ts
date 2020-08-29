d61c3f8a31b897951489623e30fb5ebd0cca5cb4
Matias Paverini <matias.paverini@tsoftlatam.com>
2020-08-29 14:11:11 -0300

    Initial Commit

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { isGeneratedFile } from '@angular/compiler/src/aot/util';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+
+  username:string;
+
+  password:string;
+
+  flag:boolean;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  login() :void {
+    
+    let username:string = (document.getElementById('email') as HTMLInputElement).value;
+    let password:string = (document.getElementById('pass') as HTMLInputElement).value;
+
+    if(this.isValidACcount(username, password)) {
+      console.log("Auth exitoso!");
+    }
+    else {
+      console.log("Auth failed!");
+    }
+  }
+
+  isValidACcount(user:string, pass:string) :boolean {
+    this.flag = false;
+    if(this.isValidUser(user) && this.isValidPass(pass)){
+      this.flag = true;
+    }
+    return this.flag;
+  }
+
+  isValidUser(user:string) :boolean {
+    let flag:boolean = false;
+    if(user.includes('@')) {
+      flag = true;
+    }
+    return flag;
+  }
+
+  isValidPass(pass:string) :boolean {
+    let flag:boolean = false;
+    if(pass.length > 4) {
+      flag = true;
+    }
+    return flag;
+  }
+
+}
